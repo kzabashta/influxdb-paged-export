2f60e8ada492a08535a4aaf6dd846c2d868da725
Kosta Zabashta <kosta.zabashta@gmail.com>
2017-07-19 02:06:17 +0000

    MVP functionality added

--- deflux.py
+++ deflux.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+# built-in stuff
+import os
+import shutil
+import json
+
+# third party stuff
 import click
 from influxdb import InfluxDBClient
+
+def init(outdir):
+    # remove the directory with all its contents
+    if os.path.exists(outdir):
+        shutil.rmtree(outdir)
+    os.makedirs(outdir)
 
 @click.command()
 @click.option('--host', prompt='InfluxDB hostname',
@@ -19,8 +31,9 @@ from influxdb import InfluxDBClient
               help='Measurement to extract.')
 @click.option('--pagesize', default=10000, 
               help='Number of points to extract at a time')
-
-def extract_data(host, port, user, password, dbname, measurement, pagesize):
+@click.option('--outdir', default='./out', 
+              help='Output directory')
+def extract_data(host, port, user, password, dbname, measurement, pagesize, outdir):
     """
     Exracts points from InfluxDB in a paginated manner to prevent
     the service from consuming too much memory from caching the results.
@@ -32,11 +45,21 @@ def extract_data(host, port, user, password, dbname, measurement, pagesize):
     client = InfluxDBClient(host, port, user, password, dbname)
     offset = 0
 
-    while True:
-        result = client.query(QUERY_TEMPLATE % (measurement, pagesize, offset)).get_points()
-        offset += pagesize
-        print(list(result))
+    # initialize the CLI
+    init(outdir)
 
+    while True:
+        query = QUERY_TEMPLATE % (measurement, pagesize, offset)
+        result = list(client.query(query).get_points())
+        save_page(result, offset, outdir)
+        if len(result) == 0:
+            break
+        offset += pagesize
+
+def save_page(points, offset, outdir):
+    click.echo('saving at offset %i' % offset)
+    with open(os.path.join(outdir, '%i.json' % offset), 'w') as f:
+        json.dump(points, f)
 
 if __name__ == '__main__':
     extract_data()
